f86c4ba6c7ff9e1f9a5cba96ed659723527d028a
Andrew Beltrano <abeltrano@gmail.com>
2023-11-06 21:40:58 -0700

    Add WifiConfigureAccessPoint C++ unit test.

--- tests/unit/TestNetRemoteServiceClient.cxx
+++ tests/unit/TestNetRemoteServiceClient.cxx
@@ -1,5 +1,7 @@
 
+#include <array>
 #include <cstdint>
+#include <format>
 #include <iostream>
 
 #include <catch2/catch_test_macros.hpp>
@@ -12,6 +14,26 @@ namespace detail
 {
 constexpr auto RemoteServiceAddressHttp = "localhost:5047";
 [[maybe_unused]] constexpr auto RemoteServiceAddressHttps = "localhost:7073";
+
+// TODO: use magic_enum instead
+static constexpr std::array<Microsoft::Net::Wifi::RadioBand, 3> AllRadioBands = {
+    Microsoft::Net::Wifi::RadioBand::RadioBandUnknown,
+    Microsoft::Net::Wifi::RadioBand::RadioBand2400MHz,
+    Microsoft::Net::Wifi::RadioBand::RadioBand5000MHz,
+};
+
+// TODO: use magic_enum instead
+static constexpr std::array<Microsoft::Net::Wifi::Dot11PhyType, 9> AllPhyTypes = {
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeUnknown,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeB,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeG,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeN,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeA,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeAC,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeAD,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeAX,
+  Microsoft::Net::Wifi::Dot11PhyType::Dot11PhyTypeBE,
+};
 } // namespace detail
 
 TEST_CASE("net remote service can be reached", "[basic][rpc][client]")
@@ -33,10 +55,34 @@ TEST_CASE("net remote service can be reached", "[basic][rpc][client]")
 
         auto status = client->GetWifiApControl(&clientContext, request, &response);
 
-        std::cout << std::format("status={} {} {}\n", static_cast<uint32_t>(status.error_code()), status.error_message(), status.error_details());
-
         REQUIRE(status.ok()); 
         REQUIRE(response.requestid() == request.id());
         REQUIRE(response.payload() == "Pong");
+    }
+
+    SECTION("WifiConfigureAccessPoint can be called")
+    {
+        for (const auto& band : detail::AllRadioBands)
+        {
+            for (const auto& phyType : detail::AllPhyTypes)
+            {
+                Microsoft::Net::Wifi::AccessPointConfiguration apConfiguration{};
+                apConfiguration.set_phytype(phyType);
+
+                Microsoft::Net::Remote::Wifi::WifiConfigureAccessPointRequest request{};
+                request.set_accesspointid(std::format("TestWifiConfigureAccessPoint"));
+                request.set_defaultband(band);
+                request.mutable_configurations()->emplace(band, apConfiguration);
+
+                Microsoft::Net::Remote::Wifi::WifiConfigureAccessPointResult result{};
+                grpc::ClientContext clientContext{};
+
+                auto status = client->WifiConfigureAccessPoint(&clientContext, request, &result);
+
+                REQUIRE(status.ok());
+                REQUIRE(result.succeeded() == true);
+                REQUIRE(result.accesspointid() == request.accesspointid());
+            }
+        }
     }
 }
