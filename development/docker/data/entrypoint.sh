ea298233a9e55fcfa199ebd376f13fd77472751c
Andrew Beltrano <abeltrano@gmail.com>
2023-11-09 16:30:09 -0700

    Add docker development environment.

---
+++ development/docker/data/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+SSH_HOME=~/.ssh
+
+if [ -d /tmp/.ssh ]; then
+    echo "Configuring host ssh keys"
+    install -d -m 700 -o ${USER} -g ${USER} ~/
+    cp -r /tmp/.ssh ~/.ssh/
+    if [ -f ${SSH_HOME}/id_rsa.pub ]; then
+        chmod 644 ${SSH_HOME}/id_rsa.pub
+    fi
+    if [ -f ${SSH_HOME}/id_rsa ]; then
+        chmod 600 ${SSH_HOME}/id_rsa
+    fi
+else 
+    echo "No host ssh keys found, skipping configuration"
+fi
+
+if [ -f /tmp/.gitconfig ]; then
+    install -m 644 -o ${USER} -g ${USER} /tmp/.gitconfig ~/
+fi
+
+if [ -x /usr/bin/neofetch ]; then
+    /usr/bin/neofetch
+fi 
+
+exec "$@"
