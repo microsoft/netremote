244b2a440e4ce1700a5829c4bddd9fd0cc5a22e0
Andrew Beltrano <abeltrano@gmail.com>
2023-11-10 15:34:47 -0700

    Update entrypoint script to avoid copying ssh keys if already done (eg. via devcontainer).

--- development/docker/data/entrypoint.sh
+++ development/docker/data/entrypoint.sh
@@ -5,14 +5,18 @@ set -euf -o pipefail
 SSH_HOME=~/.ssh
 
 if [ -d /tmp/.ssh ]; then
-    echo "Configuring host ssh keys"
-    install -d -m 700 -o ${USER} -g ${USER} ~/
-    cp -r /tmp/.ssh ~/.ssh/
-    if [ -f ${SSH_HOME}/id_rsa.pub ]; then
-        chmod 644 ${SSH_HOME}/id_rsa.pub
-    fi
-    if [ -f ${SSH_HOME}/id_rsa ]; then
-        chmod 600 ${SSH_HOME}/id_rsa
+    if [ ! -d ~/.ssh ]; then
+        echo "Configuring host ssh keys"
+        install -d -m 700 -o ${USER} -g ${USER} ~/
+        cp -r /tmp/.ssh ~/.ssh/
+        if [ -f ${SSH_HOME}/id_rsa.pub ]; then
+            chmod 644 ${SSH_HOME}/id_rsa.pub
+        fi
+        if [ -f ${SSH_HOME}/id_rsa ]; then
+            chmod 600 ${SSH_HOME}/id_rsa
+        fi
+    else
+        echo "Host ssh keys already configured"
     fi
 else 
     echo "No host ssh keys found, skipping configuration"
