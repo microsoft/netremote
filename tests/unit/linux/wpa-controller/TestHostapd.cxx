2895107d73b11f304870f86367c0a7be3e7e9d80
Andrew Beltrano <abeltrano@gmail.com>
2023-11-21 12:09:54 -0700

    Add basic tests.

--- tests/unit/linux/wpa-controller/TestHostapd.cxx
+++ tests/unit/linux/wpa-controller/TestHostapd.cxx
@@ -6,7 +6,7 @@
 
 #include "detail/WpaDaemonManager.hxx"
 
-TEST_CASE("Create a Hostapd", "[wpa][hostapd][client][remote]")
+TEST_CASE("Create a Hostapd instance", "[wpa][hostapd][client][remote]")
 {
     using namespace Wpa;
 
@@ -16,9 +16,42 @@ TEST_CASE("Create a Hostapd", "[wpa][hostapd][client][remote]")
         REQUIRE_NOTHROW(hostapd.emplace(WpaDaemonManager::InterfaceNameDefault));
     }
 
+    SECTION("Create multiple for same interface doesn't cause a crash")
+    {
+        std::optional<Hostapd> hostapd1;
+        REQUIRE_NOTHROW(hostapd1.emplace(WpaDaemonManager::InterfaceNameDefault));
+
+        std::optional<Hostapd> hostapd2;
+        REQUIRE_NOTHROW(hostapd2.emplace(WpaDaemonManager::InterfaceNameDefault));
+    }
+
+    SECTION("Sending command on invalid interface fails")
+    {
+        Hostapd hostapd("invalid");
+        REQUIRE_FALSE(hostapd.Ping());
+    }
+
     SECTION("Create reflects correct interface name")
     {
         Hostapd hostapd(WpaDaemonManager::InterfaceNameDefault);
         REQUIRE(hostapd.GetInterface() == WpaDaemonManager::InterfaceNameDefault);
+    }
+}
+
+TEST_CASE("Send Ping() command", "[wpa][hostapd][client][remote]")
+{
+    using namespace Wpa;
+
+    Hostapd hostapd(WpaDaemonManager::InterfaceNameDefault);
+
+    SECTION("Send Ping() command")
+    {
+        REQUIRE(hostapd.Ping());
+    }
+
+    SECTION("Send Ping() command on second instance")
+    {
+        Hostapd hostapd2(WpaDaemonManager::InterfaceNameDefault);
+        REQUIRE(hostapd2.Ping());
     }
 }
