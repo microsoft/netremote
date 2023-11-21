e98f50b4bfc385a63d7bc9a5f640c63d5624624e
Andrew Beltrano <abeltrano@gmail.com>
2023-11-21 14:48:11 -0700

    Add empty hostapd protocol object.

---
+++ linux/wpa-controller/ProtocolHostapd.cxx
@@ -0,0 +1,4 @@
+
+#include <Wpa/ProtocolHostapd.hxx>
+
+using namespace Wpa;
---
+++ linux/wpa-controller/include/Wpa/ProtocolHostapd.hxx
@@ -0,0 +1,16 @@
+
+#ifndef HOSTAPD_PROTOCOL_HXX
+#define HOSTAPD_PROTOCOL_HXX
+
+#include <Wpa/ProtocolWpa.hxx>
+
+namespace Wpa
+{
+struct ProtocolHostapd :
+    public ProtocolWpa
+{
+
+};
+} // namespace Wpa
+
+#endif // HOSTAPD_PROTOCOL_HXX
