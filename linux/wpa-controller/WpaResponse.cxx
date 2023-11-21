7a1ed95e91e45c8d5d70a4b7a49773293bec2499
Andrew Beltrano <abeltrano@gmail.com>
2023-11-21 14:28:31 -0700

    Add helpers to WpaResponse.

--- linux/wpa-controller/WpaResponse.cxx
+++ linux/wpa-controller/WpaResponse.cxx
@@ -1,9 +1,26 @@
 
 #include <Wpa/WpaResponse.hxx>
+#include <Wpa/ProtocolWpa.hxx>
 
 using namespace Wpa;
 
 WpaResponse::WpaResponse(std::string_view payload) :
-    Payload(payload)
+    m_payload(payload),
+    Payload(m_payload)
 {
+}
+
+WpaResponse::operator bool() const
+{
+    return IsOk();
+}
+
+bool WpaResponse::IsOk() const
+{
+    return (ProtocolWpa::IsResponseOk(Payload));
+}
+
+bool WpaResponse::Failed() const
+{
+    return (ProtocolWpa::IsResponseFail(Payload));
 }
--- linux/wpa-controller/include/Wpa/WpaResponse.hxx
+++ linux/wpa-controller/include/Wpa/WpaResponse.hxx
@@ -13,12 +13,60 @@ namespace Wpa
  */
 struct WpaResponse
 {
+    /**
+     * @brief Destroy the WpaResponse object.
+     */
     virtual ~WpaResponse() = default;
 
+    /**
+     * @brief Construct an empty WpaResponse object.
+     * 
+     */
     WpaResponse() = default;
+
+    /**
+     * @brief Construct a WpaResponse object with the specified payload.
+     * 
+     * @param payload The payload of a WPA control command response.
+     */
     WpaResponse(std::string_view payload);
 
-    std::string Payload;
+    /**
+     * @brief Implicit bool operator allowing WpaResponse to be used directly in
+     * condition statements (eg. if (response) // ...).
+     * 
+     * @return true 
+     * @return false 
+     */
+    operator bool() const;
+
+    /**
+     * @brief Indicates whether the response describes a successful result.
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool IsOk() const;
+
+    /**
+     * @brief Indicates whether the response describes a failed result.
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool Failed() const;
+
+private:
+    /**
+     * @brief Note: this must be declared prior to Payload since Payload is
+     * initialized with its contents and the constructor initialization order
+     * follows the order of declaration irrespective of the order of
+     * initialization in the constructor.
+     */
+    const std::string m_payload;
+
+public:
+    std::string_view Payload;
 };
 } // namespace Wpa
 
