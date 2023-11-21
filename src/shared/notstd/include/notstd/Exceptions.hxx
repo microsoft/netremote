3bf0f6a63b732a1a1ff856d4dc636ded9159e6ac
Andrew Beltrano <abeltrano@gmail.com>
2023-11-21 11:13:33 -0700

    Add notstd lib and NotImplementedException.

---
+++ src/shared/notstd/include/notstd/Exceptions.hxx
@@ -0,0 +1,24 @@
+
+#ifndef EXCEPTIONS_HXX
+#define EXCEPTIONS_HXX
+
+#include <format>
+#include <source_location>
+#include <stdexcept>
+
+namespace notstd
+{
+/**
+ * @brief Simple exception to indicate a function is not implemented.
+ */
+struct NotImplementedException :
+    public std::logic_error
+{
+    NotImplementedException(std::source_location location = std::source_location::current(), std::string_view message = "") :
+        std::logic_error(std::format("{}({}:{}) `{}` not implemented {}", location.file_name(), location.line(), location.column(), location.function_name(), (!std::empty(message) ? std::string(": ").append(message) : "")))
+    {
+    }
+};
+} // namespace notstd
+
+#endif // EXCEPTIONS_HXX
