29ea42f8c13f9d138b19d4eee5463ff83d549fc0
Andrew Beltrano <abeltrano@gmail.com>
2023-11-22 16:52:57 -0700

    Add docs.

--- linux/wpa-controller/include/Wpa/WpaCommand.hxx
+++ linux/wpa-controller/include/Wpa/WpaCommand.hxx
@@ -25,12 +25,25 @@ struct WpaCommand :
     {
     }
 
+    /**
+     * @brief Parse the response payload into a WpaResponse object.
+     * 
+     * @param responsePayload The response payload to parse.
+     * @return std::shared_ptr<WpaResponse> 
+     */
     std::shared_ptr<WpaResponse>
     ParseResponse(std::string_view responsePayload) const;
 
     std::string Data;
 
 private:
+    /**
+     * @brief Create a response parser object for the given command.
+     * 
+     * @param command The command to create a response parser for.
+     * @param responsePayload The response payload to parse.
+     * @return std::unique_ptr<WpaResponseParser> 
+     */
     std::unique_ptr<WpaResponseParser> CreateResponseParser(const WpaCommand* command, std::string_view responsePayload) const override;
 };
 } // namespace Wpa
