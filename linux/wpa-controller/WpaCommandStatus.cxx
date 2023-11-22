08d60e4baf2beea1f778cea14cd7d4fc679b44e7
Andrew Beltrano <abeltrano@gmail.com>
2023-11-22 16:13:49 -0700

    Remove debug prints.

--- linux/wpa-controller/WpaCommandStatus.cxx
+++ linux/wpa-controller/WpaCommandStatus.cxx
@@ -1,6 +1,4 @@
 
-#include <iostream> // TODO: remove me
-
 #include <Wpa/ProtocolHostapd.hxx>
 #include <Wpa/ProtocolWpa.hxx>
 #include <Wpa/WpaCommandStatus.hxx>
@@ -10,7 +8,6 @@ using namespace Wpa;
 
 std::unique_ptr<WpaResponseParser> WpaCommandStatus::CreateResponseParser(const WpaCommand* command, std::string_view responsePayload) const
 {
-    std::cout << "WpaCommandStatus::CreateResponseParser() called\n";
     return std::make_unique<WpaStatusResponseParser>(command, responsePayload);
 }
 
@@ -19,13 +16,10 @@ WpaStatusResponseParser::WpaStatusResponseParser(const WpaCommand* command, std:
         { ProtocolHostapd::ResponseStatusPropertyKeyState, WpaValuePresence::Required },
     })
 {
-    std::cout << "WpaStatusResponseParser::WpaStatusResponseParser() called\n";
 }
 
 std::shared_ptr<WpaResponse> WpaStatusResponseParser::ParsePayload()
 {
-    std::cout << "WpaStatusResponseParser::ParsePayload() called\n";
-
     const auto properties = GetProperties();
     const auto response = std::make_shared<WpaResponseStatus>();
 
