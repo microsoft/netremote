d240faa34f812c0ced7cd6236141f7e95bec730a
Andrew Beltrano <abeltrano@gmail.com>
2023-11-30 15:17:45 -0700

    Convert netremote-server from exe to static lib.

---
+++ src/server/NetRemoteServer.cxx
@@ -0,0 +1,44 @@
+
+#include <format>
+#include <iostream>
+
+#include <grpcpp/server_builder.h>
+#include <microsoft/net/remote/NetRemoteServer.hxx>
+
+using namespace Microsoft::Net::Remote;
+
+NetRemoteServer::NetRemoteServer(std::string_view serverAddress) :
+    m_serverAddress{serverAddress}
+{
+}
+
+std::unique_ptr<grpc::Server>& NetRemoteServer::GetGrpcServer() noexcept
+{
+    return m_server;
+}
+
+void NetRemoteServer::Run()
+{
+    if (m_server != nullptr)
+    {
+        return;
+    }
+
+    grpc::ServerBuilder builder{};
+    builder.AddListeningPort(m_serverAddress, grpc::InsecureServerCredentials());
+    builder.RegisterService(&m_service);
+
+    m_server = builder.BuildAndStart();
+    std::cout << std::format("Started listening on {}", m_serverAddress) << std::endl;
+}
+
+void NetRemoteServer::Stop()
+{
+    if (m_server == nullptr)
+    {
+        return;
+    }
+
+    m_server->Shutdown();
+    m_server = nullptr;
+}
---
+++ src/server/include/microsoft/net/remote/NetRemoteServer.hxx
@@ -0,0 +1,56 @@
+
+#ifndef NET_REMOTE_SERVER_HXX
+#define NET_REMOTE_SERVER_HXX
+
+#include <memory>
+#include <string_view>
+#include <string>
+
+#include <grpcpp/server.h>
+#include <microsoft/net/remote/NetRemoteService.hxx>
+
+namespace Microsoft::Net::Remote
+{
+/**
+ * @brief Represents a remote server. Note that this class is not thread-safe.
+ */
+struct NetRemoteServer
+{
+    /**
+     * @brief Default address for the server to listen on.
+     */
+    static constexpr auto ServerAddressDefault = "0.0.0.0:5047";
+
+    /**
+     * @brief Construct a new NetRemoteServer object. This does not start the
+     * server, the Run() function must be used for that.
+     * 
+     * @param serverAddress 
+     */
+    NetRemoteServer(std::string_view serverAddress = ServerAddressDefault);
+
+    /**
+     * @brief Get the GrpcServer object.
+     * 
+     * @return std::unique_ptr<grpc::Server>& 
+     */
+    std::unique_ptr<grpc::Server>& GetGrpcServer() noexcept;
+
+    /**
+     * @brief Start the server if not already started.
+     */
+    void Run();
+
+    /**
+     * @brief Stop the server if started.
+     */
+    void Stop();
+
+private:
+    std::string m_serverAddress;
+    std::unique_ptr<grpc::Server> m_server;
+    Service::NetRemoteService m_service{};
+};
+} // namespace Microsoft::Net::Remote
+
+#endif // NET_REMOTE_SERVER_HXX
