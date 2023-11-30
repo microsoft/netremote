2852e10b465331bbb3f126b5cec9670024b4c282
Andrew Beltrano <abeltrano@gmail.com>
2023-11-30 16:40:30 -0700

    Add daemonization support.

--- linux/server/Main.cxx
+++ linux/server/Main.cxx
@@ -2,16 +2,32 @@
 #include <format>
 #include <iostream>
 
+#include <microsoft/net/remote/NetRemoteServerConfiguration.hxx>
 #include <microsoft/net/remote/NetRemoteServer.hxx>
+#include <unistd.h>
 
 using namespace Microsoft::Net::Remote;
 
 int
 main(int argc, char* argv[])
 {
-    NetRemoteServer server{ argc, argv };
+    const auto configuration = NetRemoteServerConfiguration::FromCommandLineArguments(argc, argv);
+
+    NetRemoteServer server{ configuration.ServerAddress };
     server.Run();
-    server.GetGrpcServer()->Wait();
+
+    if (configuration.RunInBackground)
+    {
+        constexpr int nochdir = 0; // Change current working directory to /
+        constexpr int noclose = 0; // Don't redirect stdin, stdout to /dev/null
+
+        if (daemon(nochdir, noclose) != 0) {
+            const int error = errno;
+            const auto what = std::format("failed to daemonize (error={})", error);
+            std::cerr << what << std::endl;
+            throw std::runtime_error(what);
+        }
+    }
 
     return 0;
 }
