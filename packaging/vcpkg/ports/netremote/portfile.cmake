c8db8f5fd856b4f874319a9f7df54d44487c1f53
Andrew Beltrano <abeltrano@gmail.com>
2023-12-07 16:28:14 -0700

    Disable tests in vcpkg port build.

--- packaging/vcpkg/ports/netremote/portfile.cmake
+++ packaging/vcpkg/ports/netremote/portfile.cmake
@@ -1,14 +1,15 @@
 
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL C:/Users/corbinphipps/source/repos/netremote
-    REF 92a13dbdda126793ec1fa21c123e835e411e2749
-    FETCH_REF 
+    URL C:/src/microsoft/netremote
+    REF 799874f17d1f05ace49e019d2d8389154be18965
+    FETCH_REF user/corbinphipps/add-vcpkg-port
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DNETREMOTE_DISABLE_TESTS
         ${FEATURE_OPTIONS}
 )
 
