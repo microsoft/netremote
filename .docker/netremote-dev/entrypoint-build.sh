336d5b44ced81b6947a497e2556cd4e158727b4e
Andrew Beltrano <abeltrano@gmail.com>
2023-12-06 17:34:00 -0700

    Install vcpkg deps in container.

--- .docker/netremote-dev/entrypoint-build.sh
+++ .docker/netremote-dev/entrypoint-build.sh
@@ -2,25 +2,26 @@
 
 set -euf -o pipefail
 
-REPOSITORY_ROOT=$1
+REPOSITORY_ROOT=${1:-${PWD}}
+VCPKG_SUBMODULE_ROOT=${PWD}
 BUILD_DIR=build
+
+# Verify the repository root is a git repository.
+if [[ ! -d ${REPOSITORY_ROOT}/.git ]]; then
+  echo "Repository root is not a git repository: ${REPOSITORY_ROOT}"
+  exit 1
+fi
 
 # Assign cmake presets.
 # TODO: make these arguments, and use these as the default values?
 PRESET_CONFIGURE=dev-linux
 PRESET_BUILD=dev
 
-# Ensure the repository root exists.
-if [[ ! -d ${REPOSITORY_ROOT} ]]; then
-  echo "Repository root does not exist: ${REPOSITORY_ROOT}"
-  exit 1
-fi
-
 # Add the workspace directory to the safe directory list.
 git config --global --add safe.directory /github/workspace
 
 # Change to the root of the repo.
 cd ${REPOSITORY_ROOT}
-cmake -B ${BUILD_DIR} --preset ${PRESET_CONFIGURE} .
+cmake -B ${BUILD_DIR} --preset ${PRESET_CONFIGURE} -DVCPKG_SUBMODULE_ROOT=${VCPKG_SUBMODULE_ROOT} .
 cmake --build --preset ${PRESET_BUILD}
 cmake --install ${BUILD_DIR}
