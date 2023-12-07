39a97cbc161744cd16b3fd3115e23020d1e271fb
Andrew Beltrano <abeltrano@gmail.com>
2023-12-07 11:53:29 -0700

    Add build script changes.

--- .docker/netremote-dev/entrypoint-build.sh
+++ .docker/netremote-dev/entrypoint-build.sh
@@ -3,7 +3,6 @@
 set -euf -o pipefail
 
 REPOSITORY_ROOT=${1:-${PWD}}
-BUILD_DIR=build
 
 # Verify the repository root is a git repository.
 if [[ ! -d ${REPOSITORY_ROOT}/.git ]]; then
@@ -18,14 +17,16 @@ if [[ ! -f ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ]]; then
 fi
 
 # Assign cmake presets.
-PRESET_CONFIGURE=dev-linux
-PRESET_BUILD=${PRESET_CONFIGURE}
+# TODO: these should be passed in as arguments instead.
+PRESET_CONFIGURE=release-linux
+PRESET_BUILD=release-linux-debug
+BUILD_CONFIG=Debug
 
 # Add the workspace directory to the safe directory list.
 git config --global --add safe.directory ${REPOSITORY_ROOT}
 
 # Change to the root of the repo.
 cd ${REPOSITORY_ROOT}
-cmake -B ${BUILD_DIR} --preset ${PRESET_CONFIGURE}
-cmake --build --preset ${PRESET_BUILD}
-cmake --install
+cmake --preset ${PRESET_CONFIGURE}
+cmake --preset ${PRESET_BUILD} --build
+cmake --install --config ${BUILD_CONFIG} out
