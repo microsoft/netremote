beb4a437c2f58ff1f2875c7cc61d708bc0a6cd25
Andrew Beltrano <abeltrano@gmail.com>
2023-12-15 16:19:30 -0700

    Add access point discovery agent.

---
+++ src/common/wifi/apmanager/AccessPointDiscoveryAgent.cxx
@@ -0,0 +1,65 @@
+
+#include <microsoft/net/wifi/AccessPointDiscoveryAgent.hxx>
+#include <microsoft/net/wifi/IAccessPoint.hxx>
+
+using namespace Microsoft::Net::Wifi;
+
+AccessPointDiscoveryAgent::AccessPointDiscoveryAgent(std::function<void(AccessPointPresence presence, const std::shared_ptr<IAccessPoint> deviceChanged)> onDevicePresenceChanged) :
+    m_onDevicePresenceChanged(std::move(onDevicePresenceChanged))
+{}
+
+void AccessPointDiscoveryAgent::RegisterDiscoveryEventCallback(std::function<void(AccessPointPresence presence, const std::shared_ptr<IAccessPoint> deviceChanged)> onDevicePresenceChanged)
+{
+    std::unique_lock<std::shared_mutex> onDevicePresenceChangedLock{ m_onDevicePresenceChangedGate };
+    m_onDevicePresenceChanged = std::move(onDevicePresenceChanged);
+}
+
+void AccessPointDiscoveryAgent::DevicePresenceChanged(AccessPointPresence presence, std::shared_ptr<IAccessPoint> deviceChanged) const noexcept
+{
+    std::shared_lock<std::shared_mutex> onDevicePresenceChangedLock{ m_onDevicePresenceChangedGate };
+    if (m_onDevicePresenceChanged) 
+    {
+        m_onDevicePresenceChanged(presence, std::move(deviceChanged));
+    }
+}
+
+bool AccessPointDiscoveryAgent::IsStarted() const noexcept
+{
+    return m_started;
+}
+
+void AccessPointDiscoveryAgent::Start()
+{
+    bool expected = false;
+    if (m_started.compare_exchange_weak(expected, true)) 
+    {
+        StartImpl();
+    }
+}
+
+void AccessPointDiscoveryAgent::Stop()
+{
+    bool expected = true;
+    if (m_started.compare_exchange_weak(expected, false))
+    {
+        StopImpl();
+    }
+}
+
+std::future<std::vector<std::shared_ptr<IAccessPoint>>> AccessPointDiscoveryAgent::ProbeAsync()
+{
+    return ProbeAsyncImpl();
+}
+
+void AccessPointDiscoveryAgent::StartImpl()
+{}
+
+void AccessPointDiscoveryAgent::StopImpl()
+{}
+
+std::future<std::vector<std::shared_ptr<IAccessPoint>>> AccessPointDiscoveryAgent::ProbeAsyncImpl()
+{
+    std::promise<std::vector<std::shared_ptr<IAccessPoint>>> probePromise{};
+    probePromise.set_value({});
+    return probePromise.get_future();
+}
---
+++ src/common/wifi/apmanager/include/microsoft/net/wifi/AccessPointDiscoveryAgent.hxx
@@ -0,0 +1,117 @@
+
+#ifndef ACCESS_POINT_DISCOVERY_AGENT_HXX
+#define ACCESS_POINT_DISCOVERY_AGENT_HXX
+
+#include <atomic>
+#include <functional>
+#include <future>
+#include <memory>
+#include <shared_mutex>
+
+namespace Microsoft::Net::Wifi
+{
+struct IAccessPoint;
+
+/**
+ * @brief The presence of a an access point.
+ */
+enum class AccessPointPresence 
+{
+    Arrived,
+    Departed,
+};
+
+/**
+ * @brief Discovers and monitors IAccessPoint instances.
+ */
+struct AccessPointDiscoveryAgent
+{
+    /**
+     * @brief Construct a new AccessPointDiscoveryAgent object.
+     */
+    AccessPointDiscoveryAgent() = default;
+
+    /**
+     * @brief Destroy the AccessPointDiscoveryAgent object
+     */
+    virtual ~AccessPointDiscoveryAgent() = default;
+
+    /**
+     * @brief Construct a new AccessPointDiscoveryAgent object.
+     *
+     * @param onDevicePresenceChanged
+     */
+    explicit AccessPointDiscoveryAgent(std::function<void(AccessPointPresence presence, std::shared_ptr<IAccessPoint> deviceChanged)> onDevicePresenceChanged);
+
+    /**
+     * @brief Register a callback for device presence change events.
+     *
+     * The caller must ensure the validity of this callback during the lifetime of this object instance.
+     *
+     * @param onDevicePresenceChanged The callback to register.
+     */
+    void
+    RegisterDiscoveryEventCallback(std::function<void(AccessPointPresence presence, std::shared_ptr<IAccessPoint> deviceChanged)> onDevicePresenceChanged);
+
+    /**
+     * @brief indicates the started/running state.
+     *
+     * @return true
+     * @return false
+     */
+    bool IsStarted() const noexcept;
+
+    /**
+     * @brief Start actively discovering devices.
+     */
+    void Start();
+
+    /**
+     * @brief Stop actively discovering devices.
+     */
+    void Stop();
+
+    /**
+     * @brief Probe for all existing devices.
+     * 
+     * @return std::future<std::vector<std::shared_ptr<IAccessPoint>>> 
+     */
+    std::future<std::vector<std::shared_ptr<IAccessPoint>>> ProbeAsync();
+
+protected:
+    /**
+     * @brief Wrapper for safely invoking any device presence changed registered callback.
+     *
+     * @param presence The presence change that occurred.
+     * @param deviceChanged The device the change occurred for.
+     */
+    void DevicePresenceChanged(AccessPointPresence presence, std::shared_ptr<IAccessPoint> deviceChanged) const noexcept;
+
+protected:
+    /**
+     * @brief Derived class implementation of discovery start.
+     */
+    virtual void StartImpl();
+
+    /**
+     * @brief Derived class implementation of discovery stop.
+     */
+    virtual void StopImpl();
+
+    /**
+     * @brief Derived class implementation of asynchronous discovery probe.
+     *
+     * @return std::future<std::vector<std::shared_ptr<IAccessPoint>>>
+     */
+    virtual std::future<std::vector<std::shared_ptr<IAccessPoint>>> ProbeAsyncImpl();
+
+private:
+    std::atomic<bool> m_started{ false };
+
+    mutable std::shared_mutex m_onDevicePresenceChangedGate;
+    std::function<void(AccessPointPresence presence, std::shared_ptr<IAccessPoint> deviceChanged)> m_onDevicePresenceChanged;
+};
+
+} // namespace Microsoft::Net::Wifi
+
+#endif // ACCESS_POINT_DISCOVERY_AGENT_HXX
