173046ceaa2f792a1b00bc1c08d9850b44b80bf1
Andrew Beltrano <abeltrano@gmail.com>
2023-12-16 20:31:08 -0700

    Expand destruction tests.

--- tests/unit/wifi/apmanager/TestAccessPointManager.cxx
+++ tests/unit/wifi/apmanager/TestAccessPointManager.cxx
@@ -1,6 +1,9 @@
 
 #include <catch2/catch_test_macros.hpp>
+#include <microsoft/net/wifi/AccessPointDiscoveryAgent.hxx>
 #include <microsoft/net/wifi/AccessPointManager.hxx>
+
+#include "AccessPointDiscoveryAgentOperationsTest.hxx"
 
 TEST_CASE("Create an AccessPointManager instance", "[wifi][core][apmanager]")
 {
@@ -16,9 +19,22 @@ TEST_CASE("Destroy an AccessPointManager instance", "[wifi][core][apmanager]")
 {
     using namespace Microsoft::Net::Wifi;
 
+    using Test::AccessPointDiscoveryAgentOperationsTest;
+
     SECTION("Destroy doesn't cause a crash")
     {
         auto accessPointManager{ AccessPointManager::Create() };
+        REQUIRE_NOTHROW(accessPointManager.reset());
+    }
+
+    SECTION("Destroy doesn't cause a crash when there are registered discovery agents")
+    {
+        auto accessPointDiscoveryAgent1{ AccessPointDiscoveryAgent::Create(std::make_unique<AccessPointDiscoveryAgentOperationsTest>()) };
+        auto accessPointDiscoveryAgent2{ AccessPointDiscoveryAgent::Create(std::make_unique<AccessPointDiscoveryAgentOperationsTest>()) };
+
+        auto accessPointManager{ AccessPointManager::Create() };
+        accessPointManager->AddDiscoveryAgent(std::move(accessPointDiscoveryAgent1));
+        accessPointManager->AddDiscoveryAgent(std::move(accessPointDiscoveryAgent2));
         REQUIRE_NOTHROW(accessPointManager.reset());
     }
 }
