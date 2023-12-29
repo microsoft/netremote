5de16fc6e1e97506c4b4dfa3a87cb8df84a944ff
Andrew Beltrano <abeltrano@gmail.com>
2023-12-28 21:12:30 -0700

    Document netlink wrappers.

--- src/linux/libnl-helpers/include/microsoft/net/netlink/NetlinkMessage.hxx
+++ src/linux/libnl-helpers/include/microsoft/net/netlink/NetlinkMessage.hxx
@@ -6,13 +6,44 @@
 
 namespace Microsoft::Net::Netlink
 {
+/**
+ * @brief Heler for managing a netlink message, struct nl_msg. This class is not
+ * thread-safe.
+ */
 struct NetlinkMessage
 {
+    /**
+     * @brief The netlink message owned by this object.
+     */
     struct nl_msg *Message{ nullptr };
 
+    /**
+     * @brief Construct a new NetlinkMessage object that does not own a netlink
+     * message object.
+     */
+    NetlinkMessage() = default;
+
+    /**
+     * @brief Construct a new NetlinkMessage object that manages a pre-existing
+     * struct nl_msg object. Note that once construction is complete, this
+     * object owns the message and will free it when it is destroyed.
+     * 
+     * @param message The netlink message to manage. 
+     */
     NetlinkMessage(struct nl_msg *message);
+
+    /**
+     * @brief Destroy the NetlinkMessage object, freeing the managed netlink
+     * message if it exists.
+     */
     ~NetlinkMessage();
 
+    /**
+     * @brief Implicit conversion operator to struct nl_msg *, allowing this
+     * class to be used in netlink API calls.
+     * 
+     * @return struct nl_msg * The netlink message managed by this object.
+     */
     operator struct nl_msg *() const noexcept;
 };
 } // namespace Microsoft::Net::Netlink
--- src/linux/libnl-helpers/include/microsoft/net/netlink/NetlinkSocket.hxx
+++ src/linux/libnl-helpers/include/microsoft/net/netlink/NetlinkSocket.hxx
@@ -6,13 +6,51 @@
 
 namespace Microsoft::Net::Netlink
 {
+/**
+ * @brief Helper for managing a netlink socket, struct nl_sock. This class is
+ * not thread-safe.
+ */
 struct NetlinkSocket
 {
+    /**
+     * @brief The netlink socket owned by this object.
+     */
     struct nl_sock *Socket{ nullptr };
 
+    /**
+     * @brief Allocate a new struct nl_sock, and wrap it in a NetlinkSocket.
+     * 
+     * @return NetlinkSocket 
+     */
+    static NetlinkSocket Allocate();
+
+    /**
+     * @brief Construct a default NetlinkSocket object that does not own a
+     * netlink socket object.
+     */
+    NetlinkSocket() = default;
+
+    /**
+     * @brief Construct a new NetlinkSocket that manages a pre-existing struct
+     * nl_sock object. Note that once construction is complete, this object owns
+     * the socket and will free it when it is destroyed.
+     * 
+     * @param socket The netlink socket to manage.
+     */
     NetlinkSocket(struct nl_sock *socket);
+
+    /**
+     * @brief Destroy the NetlinkSocket object, freeing the managed netlink
+     * socket if it exists.
+     */
     ~NetlinkSocket();
 
+    /**
+     * @brief Implicit conversion operator to struct nl_sock *, allowing this
+     * class to be used in netlink API calls.
+     * 
+     * @return struct nl_sock * The netlink socket managed by this object.
+     */
     operator struct nl_sock *() const noexcept;
 };
 } // namespace Microsoft::Net::Netlink
