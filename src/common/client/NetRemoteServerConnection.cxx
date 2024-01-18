e358f7c3d986455b68b6575a83a4e4b865ef047e
Andrew Beltrano <abeltrano@gmail.com>
2024-01-17 19:51:25 -0700

    Add connection helper.

---
+++ src/common/client/NetRemoteServerConnection.cxx
@@ -0,0 +1,42 @@
+
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+#include <microsoft/net/remote/NetRemoteServerConnection.hxx>
+#include <plog/Log.h>
+
+using namespace Microsoft::Net::Remote;
+
+using namespace Microsoft::Net::Remote::Service;
+using namespace Microsoft::Net::Remote::Wifi;
+
+NetRemoteServerConnection::NetRemoteServerConnection(std::string_view address, std::shared_ptr<grpc::Channel> channel, std::unique_ptr<NetRemote::Stub> client) :
+    Address(address),
+    Channel(channel),
+    Client(std::move(client))
+{
+}
+
+/* static */
+std::optional<NetRemoteServerConnection>
+NetRemoteServerConnection::TryEstablishConnection(std::string_view address)
+{
+    // Attempt to establish a connection (channel) to the server.
+    static constexpr auto DontTryToConnect{ false };
+    auto channel = grpc::CreateChannel(std::data(address), grpc::InsecureChannelCredentials());
+    if (channel == nullptr || channel->GetState(DontTryToConnect) != GRPC_CHANNEL_IDLE) {
+        LOGE << "Failed to establish connection channel";
+        return std::nullopt;
+    }
+
+    // Attempt to create a new client API stub.
+    auto client = NetRemote::NewStub(channel);
+    if (client == nullptr) {
+        LOGE << "Failed to create NetRemote API stub";
+        return std::nullopt;
+    }
+
+    // Return a new connection object.
+    NetRemoteServerConnection netRemoteServerConnection(address, channel, std::move(client));
+    return netRemoteServerConnection;
+}
---
+++ src/common/client/include/microsoft/net/remote/NetRemoteServerConnection.hxx
@@ -0,0 +1,54 @@
+
+#ifndef NET_REMOTE_SERVER_CONNECTION
+#define NET_REMOTE_SERVER_CONNECTION
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+
+#include <grpcpp/channel.h>
+#include <microsoft/net/remote/protocol/NetRemoteService.grpc.pb.h>
+
+namespace Microsoft::Net::Remote
+{
+/**
+ * @brief Helper structure to collect server connection information.
+ */
+struct NetRemoteServerConnection
+{
+    std::string Address;
+    std::shared_ptr<grpc::Channel> Channel;
+    std::unique_ptr<Microsoft::Net::Remote::Service::NetRemote::Stub> Client;
+
+    NetRemoteServerConnection(const NetRemoteServerConnection& other) = delete;
+    NetRemoteServerConnection(NetRemoteServerConnection&& other) = default;
+
+    NetRemoteServerConnection
+    operator=(const NetRemoteServerConnection& other) = delete;
+
+    NetRemoteServerConnection&
+    operator=(NetRemoteServerConnection&& other) = default;
+
+    /**
+     * @brief Attempt to establish a basic connection to a remote server.
+     *
+     * @param address The address to use to connect. Must be in the form <host>:<port>.
+     * @return std::optional<NetRemoteServerConnection>
+     */
+    static std::optional<NetRemoteServerConnection>
+    TryEstablishConnection(std::string_view address);
+
+private:
+    /**
+     * @brief Construct a new NetRemoteServerConnection object with the specified address, channel, and client.
+     *
+     * @param address The address that was used to connect.
+     * @param channel The established channel.
+     * @param client The client stub that was obtained from 'channel'.
+     */
+    NetRemoteServerConnection(std::string_view address, std::shared_ptr<grpc::Channel> channel, std::unique_ptr<Microsoft::Net::Remote::Service::NetRemote::Stub> client);
+};
+} // namespace Microsoft::Net::Remote
+
+#endif // NET_REMOTE_SERVER_CONNECTION
