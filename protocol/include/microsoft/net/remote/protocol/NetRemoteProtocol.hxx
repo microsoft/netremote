a6cd993d4534940fe7fd621d1475e4568a01df52
Andrew Beltrano <abeltrano@gmail.com>
2024-01-17 20:21:56 -0700

    Expose protocol information from new header.

---
+++ protocol/include/microsoft/net/remote/protocol/NetRemoteProtocol.hxx
@@ -0,0 +1,36 @@
+
+#ifndef NET_REMOTE_PROTOCOL_HXX
+#define NET_REMOTE_PROTOCOL_HXX
+
+#include <cstdint>
+#include <ranges>
+#include <string_view>
+
+namespace Microsoft::Net::Remote
+{
+static constexpr uint32_t NetRemotePortDefault = 5047;
+static constexpr std::string_view NetRemoteAddressHttpDefault = "localhost:5047";
+
+/**
+ * @brief Static NetRemote protocol information.
+ */
+struct NetRemoteProtocol
+{
+#define IP_DEFAULT   "localhost"
+#define PORT_DEFAULT 5047
+#define xstr(s)      str(s)
+#define str(s)       #s
+
+    static constexpr uint32_t PortDefault{ 5047 };
+    static constexpr std::string_view IpDefault{ "localhost" };
+    static constexpr std::string_view AddressDefault{ IP_DEFAULT ":" xstr(PORT_DEFAULT) };
+
+#undef IP_DEFAULT
+#undef PORT_DEFAULT
+#undef xstr
+#undef str
+};
+
+} // namespace Microsoft::Net::Remote
+
+#endif // NET_REMOTE_PROTOCOL_HXX
