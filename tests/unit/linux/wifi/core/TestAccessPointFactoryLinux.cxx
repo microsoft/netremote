918a7ad2aaf80542f0d4eeaa8f53790b699b6c37
Andrew Beltrano <abeltrano@gmail.com>
2024-01-19 17:16:50 +0000

    Update linux wifi-core tests.

--- tests/unit/linux/wifi/core/TestAccessPointFactoryLinux.cxx
+++ tests/unit/linux/wifi/core/TestAccessPointFactoryLinux.cxx
@@ -1,6 +1,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <microsoft/net/wifi/AccessPointLinux.hxx>
+#include <microsoft/net/wifi/test/AccessPointControllerTest.hxx>
 
 namespace Microsoft::Net::Wifi::Test
 {
@@ -11,15 +12,20 @@ TEST_CASE("Create an AccessPointFactoryLinux instance", "[wifi][core][ap][linux]
 {
     using namespace Microsoft::Net::Wifi;
 
+    SECTION("Create doesn't cause a crash with null controller factory")
+    {
+        REQUIRE_NOTHROW(AccessPointFactoryLinux{ nullptr });
+    }
+
     SECTION("Create doesn't cause a crash")
     {
-        REQUIRE_NOTHROW(AccessPointFactoryLinux{});
+        REQUIRE_NOTHROW(AccessPointFactoryLinux{ std::make_unique<Test::AccessPointControllerFactoryTest>() });
     }
 
     SECTION("Create multiple doesn't cause a crash")
     {
-        REQUIRE_NOTHROW(AccessPointFactoryLinux{});
-        REQUIRE_NOTHROW(AccessPointFactoryLinux{});
+        REQUIRE_NOTHROW(AccessPointFactoryLinux{ std::make_unique<Test::AccessPointControllerFactoryTest>() });
+        REQUIRE_NOTHROW(AccessPointFactoryLinux{ std::make_unique<Test::AccessPointControllerFactoryTest>() });
     }
 }
 
@@ -27,9 +33,15 @@ TEST_CASE("Destroy an AccessPointFactoryLinux instance", "[wifi][core][ap][linux
 {
     using namespace Microsoft::Net::Wifi;
 
+    SECTION("Destroy doesn't cause a crashk with null controller factory")
+    {
+        AccessPointFactoryLinux accessPointFactory{ nullptr };
+        REQUIRE_NOTHROW(accessPointFactory.~AccessPointFactoryLinux());
+    }
+
     SECTION("Destroy doesn't cause a crash")
     {
-        AccessPointFactoryLinux accessPointFactory{};
+        AccessPointFactoryLinux accessPointFactory{ std::make_unique<Test::AccessPointControllerFactoryTest>() };
         REQUIRE_NOTHROW(accessPointFactory.~AccessPointFactoryLinux());
     }
 }
@@ -40,25 +52,25 @@ TEST_CASE("Create() function", "[wifi][core][ap][linux]")
 
     SECTION("Create() doesn't cause a crash")
     {
-        AccessPointFactoryLinux accessPointFactory{};
+        AccessPointFactoryLinux accessPointFactory{ std::make_unique<Test::AccessPointControllerFactoryTest>() };
         REQUIRE_NOTHROW(accessPointFactory.Create(Test::InterfaceNameDefault));
     }
 
     SECTION("Create() with empty/null inteface doesn't cause a crash")
     {
-        AccessPointFactoryLinux accessPointFactory{};
+        AccessPointFactoryLinux accessPointFactory{ std::make_unique<Test::AccessPointControllerFactoryTest>() };
         REQUIRE_NOTHROW(accessPointFactory.Create({}));
     }
 
     SECTION("Create() with valid input arguments returns non-nullptr instance")
     {
-        AccessPointFactoryLinux accessPointFactory{};
+        AccessPointFactoryLinux accessPointFactory{ std::make_unique<Test::AccessPointControllerFactoryTest>() };
         REQUIRE(accessPointFactory.Create(Test::InterfaceNameDefault) != nullptr);
     }
 
     SECTION("Create() instance reflects basic properties from base class")
     {
-        AccessPointFactoryLinux accessPointFactory{};
+        AccessPointFactoryLinux accessPointFactory{ std::make_unique<Test::AccessPointControllerFactoryTest>() };
         const auto accessPoint = accessPointFactory.Create(Test::InterfaceNameDefault);
         REQUIRE(accessPoint->GetInterface() == Test::InterfaceNameDefault);
     }
--- tests/unit/linux/wifi/core/TestAccessPointLinux.cxx
+++ tests/unit/linux/wifi/core/TestAccessPointLinux.cxx
@@ -1,6 +1,9 @@
 
+#include <memory>
+
 #include <catch2/catch_test_macros.hpp>
 #include <microsoft/net/wifi/AccessPointLinux.hxx>
+#include <microsoft/net/wifi/test/AccessPointControllerTest.hxx>
 
 namespace Microsoft::Net::Wifi::Test
 {
@@ -11,18 +14,24 @@ TEST_CASE("Create an AccessPointLinux instance", "[wifi][core][ap][linux]")
 {
     using namespace Microsoft::Net::Wifi;
 
+    SECTION("Create doesn't cause a crash with null controller factory")
+    {
+        std::optional<AccessPointLinux> accessPoint;
+        REQUIRE_NOTHROW(accessPoint.emplace(Test::InterfaceNameDefault, nullptr));
+    }
+
     SECTION("Create doesn't cause a crash")
     {
         std::optional<AccessPointLinux> accessPoint;
-        REQUIRE_NOTHROW(accessPoint.emplace(Test::InterfaceNameDefault));
+        REQUIRE_NOTHROW(accessPoint.emplace(Test::InterfaceNameDefault, std::make_unique<Test::AccessPointControllerFactoryTest>()));
     }
 
     SECTION("Create multiple for same interface doesn't cause a crash")
     {
         std::optional<AccessPointLinux> accessPoint1;
-        REQUIRE_NOTHROW(accessPoint1.emplace(Test::InterfaceNameDefault));
+        REQUIRE_NOTHROW(accessPoint1.emplace(Test::InterfaceNameDefault, std::make_unique<Test::AccessPointControllerFactoryTest>()));
 
         std::optional<AccessPointLinux> accessPoint2;
-        REQUIRE_NOTHROW(accessPoint2.emplace(Test::InterfaceNameDefault));
+        REQUIRE_NOTHROW(accessPoint2.emplace(Test::InterfaceNameDefault, std::make_unique<Test::AccessPointControllerFactoryTest>()));
     }
 }
