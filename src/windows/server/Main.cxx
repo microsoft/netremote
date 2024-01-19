95c39993bff9b119e212a1d16728f0f3ef4df611
Andrew Beltrano <abeltrano@gmail.com>
2024-01-19 13:50:36 -0700

    Update Windows netremote-server exe.

--- src/windows/server/Main.cxx
+++ src/windows/server/Main.cxx
@@ -1,18 +1,27 @@
 
 #include <format>
 #include <iostream>
+#include <memory>
 
 #include <microsoft/net/remote/NetRemoteServer.hxx>
 #include <microsoft/net/remote/NetRemoteServerConfiguration.hxx>
+#include <microsoft/net/wifi/AccessPoint.hxx>
+#include <microsoft/net/wifi/AccessPointManager.hxx>
 
 using namespace Microsoft::Net::Remote;
+using namespace Microsoft::Net::Wifi;
 
 int
 main(int argc, char* argv[])
 {
-    const auto configuration = NetRemoteServerConfiguration::FromCommandLineArguments(argc, argv);
+    auto configuration = NetRemoteServerConfiguration::FromCommandLineArguments(argc, argv);
+    {
+        auto accessPointFactory = std::make_unique<AccessPointFactory>(nullptr);
+        auto accessPointManager = AccessPointManager::Create(std::move(accessPointFactory));
+        configuration.AccessPointManager = accessPointManager;
+    }
 
-    NetRemoteServer server{ configuration.ServerAddress };
+    NetRemoteServer server{ std::move(configuration) };
     server.Run();
     server.GetGrpcServer()->Wait();
 
