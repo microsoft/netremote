eed327c5d4f18cff54b073dd1e6a7796be30a463
Andrew Beltrano <abeltrano@gmail.com>
2024-01-19 03:40:53 +0000

    Add access point capabilities structure.

---
+++ src/common/wifi/core/include/microsoft/net/wifi/AccessPointCapabilities.hxx
@@ -0,0 +1,15 @@
+
+#ifndef ACCESS_POINT_CAPABILITIES_HXX
+#define ACCESS_POINT_CAPABILITIES_HXX
+
+#include <microsoft/net/wifi/Ieee80211.hxx>
+
+namespace Microsoft::Net::Wifi
+{
+struct AccessPointCapabilities
+{
+
+};
+} // namespace Microsoft::Net::Wifi
+
+#endif // ACCESS_POINT_CAPABILITIES_HXX
