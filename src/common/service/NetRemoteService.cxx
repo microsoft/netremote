565d4945136d502b13e09cf7cc765152bde501f9
Andrew Beltrano <abeltrano@gmail.com>
2024-01-19 23:25:07 +0000

    Add wip.

--- src/common/service/NetRemoteService.cxx
+++ src/common/service/NetRemoteService.cxx
@@ -6,10 +6,14 @@
 
 #include <microsoft/net/remote/NetRemoteService.hxx>
 #include <microsoft/net/wifi/IAccessPoint.hxx>
+#include <microsoft/net/wifi/IAccessPointController.hxx>
 #include <plog/Log.h>
 
 using namespace Microsoft::Net::Remote::Service;
+
 using Microsoft::Net::Wifi::AccessPointManager;
+using Microsoft::Net::Wifi::IAccessPoint;
+using Microsoft::Net::Wifi::AccessPointControllerException;
 
 NetRemoteService::NetRemoteService(std::shared_ptr<AccessPointManager> accessPointManager) :
     m_accessPointManager(std::move(accessPointManager))
@@ -21,6 +25,89 @@ NetRemoteService::GetAccessPointManager() noexcept
     return m_accessPointManager;
 }
 
+namespace detail
+{
+Microsoft::Net::Wifi::AccessPointCapabilities
+IeeeAccessPointCapabilitiesToNetRemoteAccessPointCapabilities([[maybe_unused]] const Microsoft::Net::Wifi::Ieee80211AccessPointCapabilities& ieeeCapabilities)
+{
+    using Microsoft::Net::Wifi::AccessPointCapabilities;
+    using Microsoft::Net::Wifi::Ieee80211AccessPointCapabilities;
+
+    AccessPointCapabilities capabilities{};
+
+    // capabilities.set_protocols(ieeeCapabilities.Protocols);
+    // capabilities.set_frequencybands(ieeeCapabilities.FrequencyBands);
+    // capabilities.set_authenticationalgorithms(ieeeCapabilities.AuthenticationAlgorithms);
+    // capabilities.set_encryptionalgorithms(ieeeCapabilities.EncryptionAlgorithms);
+
+    return capabilities;
+}
+
+using Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResultItem;
+using Microsoft::Net::Wifi::AccessPointCapabilities;
+
+static constexpr auto AccessPointIdInvalid{ "invalid" };
+
+Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResultItem
+MakeInvalidAccessPointResultItem()
+{
+    WifiEnumerateAccessPointsResultItem item{};
+    item.set_accesspointid(AccessPointIdInvalid);
+    return item;
+}
+
+Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResultItem
+IAccessPointToNetRemoteAccessPointResultItem(IAccessPoint& accessPoint)
+{
+    WifiEnumerateAccessPointsResultItem item{};
+
+    bool isEnabled{ false };
+    std::string id{};
+    Microsoft::Net::Wifi::AccessPointCapabilities capabilities{};
+
+    auto interfaceName = accessPoint.GetInterfaceName();
+    id.assign(std::cbegin(interfaceName), std::cend(interfaceName));
+
+    auto accessPointController = accessPoint.CreateController();
+    if (accessPointController != nullptr) {
+        LOGE << std::format("Failed to create controller for access point {}", interfaceName);
+        return MakeInvalidAccessPointResultItem();
+    }
+
+    try {
+        auto capabilitiesIeee80211 = accessPointController->GetCapabilities();
+        capabilities = IeeeAccessPointCapabilitiesToNetRemoteAccessPointCapabilities(capabilitiesIeee80211);
+    } catch (const AccessPointControllerException& apce) {
+        LOGE << std::format("Failed to get capabilities for access point {} ({})", interfaceName, apce.what());
+        return MakeInvalidAccessPointResultItem();
+    }
+
+    // Populate the result item.
+    item.set_accesspointid(std::move(id));
+    item.set_isenabled(isEnabled);
+    *item.mutable_capabilities() = std::move(capabilities);
+
+    return item;
+}
+
+Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResultItem
+IAccessPointWeakToNetRemoteAccessPointResultItem(std::weak_ptr<Microsoft::Net::Wifi::IAccessPoint>& accessPointWeak)
+{
+    using Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResultItem;
+
+    WifiEnumerateAccessPointsResultItem item{};
+
+    auto accessPoint = accessPointWeak.lock();
+    if (accessPoint != nullptr) {
+        item = IAccessPointToNetRemoteAccessPointResultItem(*accessPoint.get());
+    } else {
+        item.set_accesspointid(AccessPointIdInvalid);
+    }
+
+    return item;
+}
+} // namespace detail
+
 ::grpc::Status
 NetRemoteService::WifiEnumerateAccessPoints([[maybe_unused]] ::grpc::ServerContext* context, [[maybe_unused]] const ::Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsRequest* request, ::Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResult* response)
 {
@@ -30,15 +117,8 @@ NetRemoteService::WifiEnumerateAccessPoints([[maybe_unused]] ::grpc::ServerConte
 
     auto accessPoints = m_accessPointManager->GetAllAccessPoints();
     std::vector<WifiEnumerateAccessPointsResultItem> accessPointResultItems(std::size(accessPoints));
-    std::ranges::transform(accessPoints, std::begin(accessPointResultItems), [](const auto& accessPointWeak) {
-        WifiEnumerateAccessPointsResultItem item{};
-        auto accessPoint = accessPointWeak.lock();
-        if (accessPoint != nullptr) {
-            auto interfaceName = accessPoint->GetInterfaceName();
-            std::string accessPointId{ std::cbegin(interfaceName), std::cend(interfaceName) };
-            item.set_accesspointid(std::move(accessPointId));
-        }
-        return std::move(item);
+    std::ranges::transform(accessPoints, std::begin(accessPointResultItems), [](auto& accessPointWeak) {
+        return detail::IAccessPointWeakToNetRemoteAccessPointResultItem(accessPointWeak);
     });
 
     *response->mutable_accesspoints() = {
