472ef8e1a7901deec9184b01780ba8883aed9c51
Andrew Beltrano <abeltrano@gmail.com>
2024-02-02 17:10:31 +0000

    Add parsing for ieee80211* status values.

--- src/linux/wpa-controller/WpaCommandStatus.cxx
+++ src/linux/wpa-controller/WpaCommandStatus.cxx
@@ -4,6 +4,8 @@
 #include <Wpa/WpaCommandStatus.hxx>
 #include <Wpa/WpaResponseStatus.hxx>
 
+#include "WpaParsingUtilities.hxx"
+
 using namespace Wpa;
 
 std::unique_ptr<WpaResponseParser>
@@ -16,6 +18,12 @@ WpaCommandStatus::CreateResponseParser(const WpaCommand* command, std::string_vi
 WpaStatusResponseParser::WpaStatusResponseParser(const WpaCommand* command, std::string_view responsePayload) :
     WpaResponseParser(command, responsePayload, {
         { ProtocolHostapd::ResponseStatusPropertyKeyState, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyIeee80211N, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyDisable11N, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyIeee80211AC, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyDisableAC, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyIeee80211AX, WpaValuePresence::Required },
+        { ProtocolHostapd::ResponseStatusPropertyKeyDisableAX, WpaValuePresence::Required }
     })
 // clang-format on
 {
@@ -24,13 +32,40 @@ WpaStatusResponseParser::WpaStatusResponseParser(const WpaCommand* command, std:
 std::shared_ptr<WpaResponse>
 WpaStatusResponseParser::ParsePayload()
 {
+    using namespace Wpa::Parsing;
+
     const auto properties = GetProperties();
     const auto response = std::make_shared<WpaResponseStatus>();
+    auto& status = response->Status;
 
     for (const auto& [key, value] : properties) {
         if (key == ProtocolHostapd::ResponseStatusPropertyKeyState) {
-            response->Status.State = HostapdInterfaceStateFromString(value);
+            status.State = HostapdInterfaceStateFromString(value);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyIeee80211N) {
+            ParseInt(value, status.Ieee80211n);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyDisable11N) {
+            ParseInt(value, status.Disable11n);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyIeee80211AC) {
+            ParseInt(value, status.Ieee80211ac);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyDisableAC) {
+            ParseInt(value, status.Disable11ac);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyIeee80211AX) {
+            ParseInt(value, status.Ieee80211ax);
+        } else if (key == ProtocolHostapd::ResponseStatusPropertyKeyDisableAX) {
+            ParseInt(value, status.Disable11ax);
         }
+    }
+
+    if (!!status.Ieee80211n) {
+        // TODO: parse attributes that are preset when this value is set.
+    }
+
+    if (!!status.Ieee80211ac) {
+        // TODO: parse attributes that are preset when this value is set.
+    }
+
+    if (!!status.Ieee80211ax) {
+        // TODO: parse attributes that are preset when this value is set.
     }
 
     return response;
