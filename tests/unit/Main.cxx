1f87e878f94a624bfcb133787bfd5413c7848431
Andrew Beltrano <abeltrano@gmail.com>
2024-02-07 20:14:14 +0000

    Enable logging in top-level unit test.

---
+++ tests/unit/Main.cxx
@@ -0,0 +1,16 @@
+
+#include <catch2/catch_session.hpp>
+#include <plog/Appenders/ColorConsoleAppender.h>
+#include <plog/Formatters/MessageOnlyFormatter.h>
+#include <plog/Init.h>
+#include <plog/Log.h>
+
+int
+main(int argc, char* argv[])
+{
+    static plog::ColorConsoleAppender<plog::MessageOnlyFormatter> colorConsoleAppender{};
+
+    plog::init(plog::verbose, &colorConsoleAppender);
+
+    return Catch::Session().run(argc, argv);
+}
