c50985b0df5d7c28489fa54569ad5bb0ff4760b8
Andrew Beltrano <abeltrano@gmail.com>
2024-02-07 17:23:36 +0000

    Add tests.

--- tests/unit/TestNetRemoteServiceClient.cxx
+++ tests/unit/TestNetRemoteServiceClient.cxx
@@ -19,6 +19,7 @@
 namespace Microsoft::Net::Remote::Test
 {
 constexpr auto AllProtocols = magic_enum::enum_values<Microsoft::Net::Wifi::Ieee80211Protocol>();
+constexpr auto AllBands = magic_enum::enum_values<Microsoft::Net::Wifi::Ieee80211FrequencyBand>();
 } // namespace Microsoft::Net::Remote::Test
 
 using Microsoft::Net::Remote::Test::RemoteServiceAddressHttp;
@@ -166,5 +167,149 @@ TEST_CASE("WifiAccessPointSetPhyType API", "[basic][rpc][client][remote]")
         REQUIRE(setPhyTypeResult.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeSucceeded);
         REQUIRE(setPhyTypeResult.status().message().empty());
         REQUIRE(setPhyTypeResult.status().has_details() == false);
+    }
+}
+
+TEST_CASE("WifiAccessPointSetFrequencyBands API", "[basic][rpc][client][remote]")
+{
+    using namespace Microsoft::Net::Remote;
+    using namespace Microsoft::Net::Remote::Service;
+    using namespace Microsoft::Net::Remote::Test;
+    using namespace Microsoft::Net::Remote::Wifi;
+    using namespace Microsoft::Net::Wifi;
+    using namespace Microsoft::Net::Wifi::Test;
+
+    constexpr auto InterfaceNameAllBands{ "TestWifiAccessPointSetFrequencyBandsAll" };
+    constexpr auto InterfaceNameBand2_4{ "TestWifiAccessPointSetFrequencyBands2_4" };
+    constexpr auto InterfaceNameBand5_0{ "TestWifiAccessPointSetFrequencyBands5_0" };
+    constexpr auto InterfaceNameBand6_0{ "TestWifiAccessPointSetFrequencyBands6_0" };
+
+    auto apManagerTest = std::make_shared<AccessPointManagerTest>();
+
+    Ieee80211AccessPointCapabilities apCapabilitiesBandsAll{
+        .FrequencyBands{ std::cbegin(AllBands), std::cend(AllBands) }
+    };
+    Ieee80211AccessPointCapabilities apCapabilitiesBands2_4{
+        .FrequencyBands{ Ieee80211FrequencyBand::TwoPointFourGHz }
+    };
+    Ieee80211AccessPointCapabilities apCapabilitiesBands5_0{
+        .FrequencyBands{ Ieee80211FrequencyBand::FiveGHz }
+    };
+    Ieee80211AccessPointCapabilities apCapabilitiesBands6_0{
+        .FrequencyBands{ Ieee80211FrequencyBand::SixGHz }
+    };
+
+    auto apTestBandsAll = std::make_shared<AccessPointTest>(InterfaceNameAllBands, std::move(apCapabilitiesBandsAll));
+    auto apTestBands2_4 = std::make_shared<AccessPointTest>(InterfaceNameBand2_4, std::move(apCapabilitiesBands2_4));
+    auto apTestBands5_0 = std::make_shared<AccessPointTest>(InterfaceNameBand5_0, std::move(apCapabilitiesBands5_0));
+    auto apTestBands6_0 = std::make_shared<AccessPointTest>(InterfaceNameBand6_0, std::move(apCapabilitiesBands6_0));
+
+    apManagerTest->AddAccessPoint(apTestBandsAll);
+    apManagerTest->AddAccessPoint(apTestBands2_4);
+    apManagerTest->AddAccessPoint(apTestBands5_0);
+    apManagerTest->AddAccessPoint(apTestBands6_0);
+
+    NetRemoteServerConfiguration Configuration{
+        .ServerAddress = RemoteServiceAddressHttp,
+        .AccessPointManager = apManagerTest,
+    };
+
+    NetRemoteServer server{ Configuration };
+    server.Run();
+
+    auto channel = grpc::CreateChannel(RemoteServiceAddressHttp, grpc::InsecureChannelCredentials());
+    auto client = NetRemote::NewStub(channel);
+
+    SECTION("Can be called")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameAllBands);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand2_4GHz);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand5_0GHz);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeSucceeded);
+    }
+
+    SECTION("Empty frequency bands returns an error")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameAllBands);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeInvalidParameter);
+    }
+
+    SECTION("Invalid frequency band returns an error")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameAllBands);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBandUnknown);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeInvalidParameter);
+    }
+
+    SECTION("Invalid frequency band with other valid bands returns an error")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameAllBands);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBandUnknown);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand5_0GHz);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeInvalidParameter);
+    }
+
+    SECTION("Unsupported frequency band returns an error")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameBand2_4);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand5_0GHz);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeOperationNotSupported);
+    }
+
+    SECTION("Enabling multiple bands works")
+    {
+        WifiAccessPointSetFrequencyBandsRequest request{};
+        request.set_accesspointid(InterfaceNameAllBands);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand2_4GHz);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand5_0GHz);
+        request.mutable_frequencybands()->Add(Dot11FrequencyBand::Dot11FrequencyBand6_0GHz);
+
+        grpc::ClientContext clientContext{};
+        WifiAccessPointSetFrequencyBandsResult result{};
+        auto status = client->WifiAccessPointSetFrequencyBands(&clientContext, request, &result);
+        REQUIRE(status.ok());
+        REQUIRE(result.accesspointid() == request.accesspointid());
+        REQUIRE(result.has_status());
+        REQUIRE(result.status().code() == WifiAccessPointOperationStatusCode::WifiAccessPointOperationStatusCodeSucceeded);
     }
 }
