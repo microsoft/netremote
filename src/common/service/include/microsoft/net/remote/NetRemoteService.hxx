327922bbfd45a32d33337aa1bceeeb5132eacc1d
Andrew Beltrano <abeltrano@gmail.com>
2024-02-08 17:46:07 +0000

    Add API function docs.

--- src/common/service/include/microsoft/net/remote/NetRemoteService.hxx
+++ src/common/service/include/microsoft/net/remote/NetRemoteService.hxx
@@ -32,25 +32,83 @@ public:
     GetAccessPointManager() noexcept;
 
 private:
+    /**
+     * @brief Enumerate the available access points.
+     *
+     * @param context
+     * @param request
+     * @param response
+     * @return grpc::Status
+     */
     virtual grpc::Status
     WifiEnumerateAccessPoints(grpc::ServerContext* context, const Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsRequest* request, Microsoft::Net::Remote::Wifi::WifiEnumerateAccessPointsResult* response) override;
 
+    /**
+     * @brief Enable an access point. This brings the access point online, making it available for use by clients.
+     *
+     * @param context
+     * @param request
+     * @param response
+     * @return grpc::Status
+     */
     virtual grpc::Status
     WifiAccessPointEnable(grpc::ServerContext* context, const Microsoft::Net::Remote::Wifi::WifiAccessPointEnableRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointEnableResult* response) override;
 
+    /**
+     * @brief Disable an access point. This will take the access point offline, making it unavailable for use by clients.
+     *
+     * @param context
+     * @param request
+     * @param response
+     * @return grpc::Status
+     */
     virtual grpc::Status
     WifiAccessPointDisable(grpc::ServerContext* context, const Microsoft::Net::Remote::Wifi::WifiAccessPointDisableRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointDisableResult* response) override;
 
+    /**
+     * @brief Set the active PHY type or protocol of the access point. The access point must be enabled. This will cause
+     * the access point to temporarily go offline while the change is being applied.
+     *
+     * @param context
+     * @param request
+     * @param response
+     * @return grpc::Status
+     */
     virtual grpc::Status
     WifiAccessPointSetPhyType(grpc::ServerContext* context, const Microsoft::Net::Remote::Wifi::WifiAccessPointSetPhyTypeRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointSetPhyTypeResult* response) override;
 
+    /**
+     * @brief Set the active frequency bands of the access point. The access point must be enabled. This will cause
+     * the access point to temporarily go offline while the change is being applied.
+     *
+     * @param context
+     * @param request
+     * @param response
+     * @return grpc::Status
+     */
     virtual grpc::Status
     WifiAccessPointSetFrequencyBands(grpc::ServerContext* context, const Microsoft::Net::Remote::Wifi::WifiAccessPointSetFrequencyBandsRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointSetFrequencyBandsResult* response) override;
 
 protected:
+    /**
+     * @brief Validate the basic input parameters for the WifiAccessPointEnable request.
+     *
+     * @param request The request to validate.
+     * @param status The status to populate with failure information.
+     * @return true
+     * @return false
+     */
     static bool
     ValidateWifiAccessPointEnableRequest(const Microsoft::Net::Remote::Wifi::WifiAccessPointEnableRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointOperationStatus& status);
 
+    /**
+     * @brief Validate the basic input parameters for the WifiAccessPointSetFrequencyBands reqest.
+     *
+     * @param request The request to validate.
+     * @param result The result to populate with failure information.
+     * @return true
+     * @return false
+     */
     static bool
     ValidateWifiSetFrequencyBandsRequest(const Microsoft::Net::Remote::Wifi::WifiAccessPointSetFrequencyBandsRequest* request, Microsoft::Net::Remote::Wifi::WifiAccessPointSetFrequencyBandsResult* result);
 
