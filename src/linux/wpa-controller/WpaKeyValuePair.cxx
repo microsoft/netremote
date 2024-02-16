7a95a9130178985a1a5ec957c0e5a9b010e471a3
Andrew Beltrano <abeltrano@gmail.com>
2024-02-16 18:31:23 +0000

    Fix static member access through variable.

--- src/linux/wpa-controller/WpaKeyValuePair.cxx
+++ src/linux/wpa-controller/WpaKeyValuePair.cxx
@@ -16,13 +16,13 @@ WpaKeyValuePair::TryParseValue(std::string_view input)
     if (!Value.has_value()) {
         // Find the starting position of the key.
         const auto keyPosition = input.find(Key);
-        if (keyPosition != input.npos) {
+        if (keyPosition != std::string_view::npos) {
             // Assign the starting position of the value, advancing past the key.
             input = input.substr(keyPosition + std::size(Key));
 
             // Find the end of the line, marking the end of the value string.
             const auto eolPosition = input.find('\n');
-            if (eolPosition != input.npos) {
+            if (eolPosition != std::string_view::npos) {
                 Value = input.substr(0, eolPosition);
             } else {
                 LOGW << std::format("Failed to parse value for key: {} (missing eol)", Key);
