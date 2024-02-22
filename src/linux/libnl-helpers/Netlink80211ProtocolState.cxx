2a58ce40b9bcbd2c8b9ba40a7f2af116328704c9
Andrew Beltrano <abeltrano@gmail.com>
2024-02-22 19:06:23 +0000

    Throw netlink-specific system_error instead of int error values.

--- src/linux/libnl-helpers/Netlink80211ProtocolState.cxx
+++ src/linux/libnl-helpers/Netlink80211ProtocolState.cxx
@@ -1,14 +1,13 @@
 
-#include <cerrno>
-#include <cstring>
 #include <format>
 #include <iterator>
+#include <system_error>
 
 #include <linux/nl80211.h>
+#include <microsoft/net/netlink/NetlinkErrorCategory.hxx>
 #include <microsoft/net/netlink/NetlinkSocket.hxx>
 #include <microsoft/net/netlink/nl80211/Netlink80211.hxx>
 #include <microsoft/net/netlink/nl80211/Netlink80211ProtocolState.hxx>
-#include <netlink/errno.h>
 #include <netlink/genl/ctrl.h>
 #include <netlink/genl/genl.h>
 #include <plog/Log.h>
@@ -25,24 +24,29 @@ Nl80211ProtocolState::Nl80211ProtocolState()
     // Connect the socket to the generic netlink family.
     const int ret = genl_connect(netlinkSocket);
     if (ret < 0) {
-        const auto err = errno;
-        LOGE << std::format("Failed to connect netlink socket for nl control with error {} ({})", err, strerror(err)); // NOLINT(concurrency-mt-unsafe)
-        throw err;
+        const auto errorCode = MakeNetlinkErrorCode(-ret);
+        const auto message = std::format("Failed to connect netlink socket for nl control with error {}", errorCode.value());
+        LOGE << message;
+        throw std::system_error(errorCode, message);
     }
 
     // Look up the nl80211 driver id.
     DriverId = genl_ctrl_resolve(netlinkSocket, NL80211_GENL_NAME);
     if (DriverId < 0) {
-        LOGE << std::format("Failed to resolve nl80211 netlink id with error {} ({})", DriverId, nl_geterror(DriverId));
-        throw DriverId;
+        const auto errorCode = MakeNetlinkErrorCode(-DriverId);
+        const auto message = std::format("Failed to resolve nl80211 netlink id with error {}", errorCode.value());
+        LOGE << message;
+        throw std::system_error(errorCode, message);
     }
 
     // Lookup the ids for the nl80211 multicast groups.
     for (const auto& [multicastGroup, multicastGroupName] : Nl80211MulticastGroupNames) {
-        int multicastGroupId = genl_ctrl_resolve_grp(netlinkSocket, NL80211_GENL_NAME, std::data(multicastGroupName));
+        const int multicastGroupId = genl_ctrl_resolve_grp(netlinkSocket, NL80211_GENL_NAME, std::data(multicastGroupName));
         if (multicastGroupId < 0) {
-            LOGE << std::format("Failed to resolve nl80211 {} multicast group id with error {} ({})", multicastGroupName, multicastGroupId, nl_geterror(multicastGroupId));
-            throw multicastGroupId;
+            const auto errorCode = MakeNetlinkErrorCode(-multicastGroupId);
+            const auto message = std::format("Failed to resolve nl80211 {} multicast group id with error {}", multicastGroupName, errorCode.value());
+            LOGE << message;
+            throw std::system_error(errorCode, message);
         }
         MulticastGroupId[multicastGroup] = multicastGroupId;
     }
