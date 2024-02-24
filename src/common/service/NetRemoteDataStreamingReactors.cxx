eb23e26defd416b66260c1fe304c03d1fee6d56a
Corbin Phipps <corbinphipps@microsoft.com>
2024-02-24 00:38:04 +0000

    Make param name consistent between header and source

--- src/common/service/NetRemoteDataStreamingReactors.cxx
+++ src/common/service/NetRemoteDataStreamingReactors.cxx
@@ -11,15 +11,15 @@ DataStreamReader::DataStreamReader(WifiDataStreamUploadResult* result) :
 }
 
 void
-DataStreamReader::OnReadDone(bool ok)
+DataStreamReader::OnReadDone(bool isOk)
 {
-    if (ok) {
+    if (isOk) {
         m_numberOfDataBlocksReceived++;
         m_readStatus.set_code(WifiDataStreamOperationStatusCode::WifiDataStreamOperationStatusCodeSucceeded);
         m_readStatus.set_message("Data read successful");
         StartRead(&m_data);
     } else {
-        // A false "ok" value could either mean a failed RPC or that no more data is available.
+        // A false "isOk" value could either mean a failed RPC or that no more data is available.
         // Unfortunately, there is no clear way to tell which situation occurred.
         m_result->set_numberofdatablocksreceived(m_numberOfDataBlocksReceived);
         *m_result->mutable_status() = std::move(m_readStatus);
