c65c433f4943633482da35ecfe202fb7f1efb46e
Corbin Phipps <corbinphipps@microsoft.com>
2024-02-26 18:40:24 +0000

    Move client reactor out of SECTION

--- tests/unit/TestNetRemoteDataStreamingServiceClient.cxx
+++ tests/unit/TestNetRemoteDataStreamingServiceClient.cxx
@@ -28,77 +28,77 @@ TEST_CASE("WifiDataStreamUpload API", "[basic][rpc][client][remote]")
     auto channel = grpc::CreateChannel(RemoteServiceAddressHttp, grpc::InsecureChannelCredentials());
     auto client = NetRemoteDataStreaming::NewStub(channel);
 
-    SECTION("Can be called")
+    class DataStreamWriter : public grpc::ClientWriteReactor<WifiDataStreamUploadData>
     {
-        class DataStreamWriter : public grpc::ClientWriteReactor<WifiDataStreamUploadData>
+    public:
+        DataStreamWriter(NetRemoteDataStreaming::Stub* client, uint32_t numberOfDataBlocksToWrite) :
+            m_numberOfDataBlocksToWrite(numberOfDataBlocksToWrite)
         {
-        public:
-            DataStreamWriter(NetRemoteDataStreaming::Stub* client, uint32_t numberOfDataBlocksToWrite) :
-                m_numberOfDataBlocksToWrite(numberOfDataBlocksToWrite)
-            {
-                client->async()->WifiDataStreamUpload(&m_clientContext, &m_result, this);
-                StartCall();
+            client->async()->WifiDataStreamUpload(&m_clientContext, &m_result, this);
+            StartCall();
+            NextWrite();
+        }
+
+        void
+        OnWriteDone(bool isOk) override
+        {
+            if (isOk) {
                 NextWrite();
+            } else {
+                StartWritesDone();
             }
+        }
 
-            void
-            OnWriteDone(bool isOk) override
-            {
-                if (isOk) {
-                    NextWrite();
-                } else {
-                    StartWritesDone();
-                }
+        void
+        OnDone(const grpc::Status& status) override
+        {
+            std::unique_lock lock(m_mutex);
+
+            m_status = status;
+            m_done = true;
+            m_cv.notify_one();
+        }
+
+        grpc::Status
+        Await(WifiDataStreamUploadResult* result)
+        {
+            std::unique_lock lock(m_mutex);
+
+            m_cv.wait(lock, [this] {
+                return m_done;
+            });
+            *result = m_result;
+
+            return m_status;
+        }
+
+    private:
+        void
+        NextWrite()
+        {
+            if (m_numberOfDataBlocksToWrite > 0) {
+                m_data.set_data(std::format("Data #{}", ++m_numberOfDataBlocksWritten));
+                StartWrite(&m_data);
+                m_numberOfDataBlocksToWrite--;
+            } else {
+                StartWritesDone();
             }
+        }
 
-            void
-            OnDone(const grpc::Status& status) override
-            {
-                std::unique_lock lock(m_mutex);
+    private:
+        grpc::ClientContext m_clientContext{};
+        WifiDataStreamUploadData m_data{};
+        WifiDataStreamUploadResult m_result{};
+        uint32_t m_numberOfDataBlocksToWrite{};
+        uint32_t m_numberOfDataBlocksWritten{};
+        grpc::Status m_status{};
+        std::mutex m_mutex{};
+        std::condition_variable m_cv{};
+        bool m_done{ false };
+    };
 
-                m_status = status;
-                m_done = true;
-                m_cv.notify_one();
-            }
-
-            grpc::Status
-            Await(WifiDataStreamUploadResult* result)
-            {
-                std::unique_lock lock(m_mutex);
-
-                m_cv.wait(lock, [this] {
-                    return m_done;
-                });
-                *result = m_result;
-
-                return m_status;
-            }
-
-        private:
-            void
-            NextWrite()
-            {
-                if (m_numberOfDataBlocksToWrite > 0) {
-                    m_data.set_data(std::format("Data #{}", ++m_numberOfDataBlocksWritten));
-                    StartWrite(&m_data);
-                    m_numberOfDataBlocksToWrite--;
-                } else {
-                    StartWritesDone();
-                }
-            }
-
-        private:
-            grpc::ClientContext m_clientContext{};
-            WifiDataStreamUploadData m_data{};
-            WifiDataStreamUploadResult m_result{};
-            uint32_t m_numberOfDataBlocksToWrite{};
-            uint32_t m_numberOfDataBlocksWritten{};
-            grpc::Status m_status{};
-            std::mutex m_mutex{};
-            std::condition_variable m_cv{};
-            bool m_done{ false };
-        };
-
+    SECTION("Can be called with one client")
+    {
         static constexpr auto numberOfDataBlocksToWrite = 10;
         auto dataStreamWriter = std::make_unique<DataStreamWriter>(client.get(), numberOfDataBlocksToWrite);
 
