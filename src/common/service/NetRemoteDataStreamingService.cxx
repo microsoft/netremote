3513304b063f0227a125a85ac316657bb60bbf02
Corbin Phipps <corbinphipps@microsoft.com>
2024-02-27 23:40:55 +0000

    Add basic API trace

--- src/common/service/NetRemoteDataStreamingService.cxx
+++ src/common/service/NetRemoteDataStreamingService.cxx
@@ -1,12 +1,16 @@
 
+#include "NetRemoteApiTrace.hxx"
 #include "NetRemoteDataStreamingReactors.hxx"
 #include <microsoft/net/remote/NetRemoteDataStreamingService.hxx>
 
 using namespace Microsoft::Net::Remote::DataStream;
 using namespace Microsoft::Net::Remote::Service;
+using namespace Microsoft::Net::Remote::Service::Tracing;
 
 grpc::ServerReadReactor<DataStreamUploadData>*
 NetRemoteDataStreamingService::DataStreamUpload([[maybe_unused]] grpc::CallbackServerContext* context, DataStreamUploadResult* result)
 {
+    const NetRemoteApiTrace traceMe{};
+
     return new Reactors::DataStreamReader(result);
 }
