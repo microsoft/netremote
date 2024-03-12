6b0bbd1c38ba93b0707a459fe1bbc008ff004438
Andrew Beltrano <abeltrano@gmail.com>
2024-03-12 13:26:03 +0000

    Move hapd-specific properties to hapd protocol file.

--- src/linux/wpa-controller/include/Wpa/ProtocolHostapd.hxx
+++ src/linux/wpa-controller/include/Wpa/ProtocolHostapd.hxx
@@ -35,6 +35,115 @@ enum class HostapdHwMode {
     Ieee80211any,
 };
 
+/**
+ * @brief WPA encoding of IEEE 802.11i-2004 protocol types.
+ *
+ * Note that wpa_supplicant/hostapd configuration don't directly encode WPA3 in this enumeration. Instead, a combination
+ * of the 'Wpa2' value below with the 'wpa_key_mgmt' property set to 'SAE' or 'FT-SAE'.
+ *
+ * Values obtained from hostap/src/common/defs.h.
+ */
+enum class WpaProtocol : uint32_t {
+    Wpa = (1U << 0U),
+    Wpa2 = (1U << 1U),
+    Wapi = (1U << 2U),
+    Osen = (1U << 3U),
+    // Aliases
+    Rsn = Wpa2,
+};
+
+static constexpr auto WpaProtocolMask = 
+    std::to_underlying(WpaProtocol::Wpa) |
+    std::to_underlying(WpaProtocol::Wpa2) |
+    std::to_underlying(WpaProtocol::Wapi) | 
+    std::to_underlying(WpaProtocol::Osen);
+
+/**
+ * @brief WPA encoding of IEEE 802.11 cipher types.
+ *
+ * Values obtained from hostap/src/common/defs.h.
+ */
+enum class WpaCipher : uint32_t {
+    None = (1U << 0U),
+    Wep40 = (1U << 1U),
+    Wep104 = (1U << 2U),
+    Tkip = (1U << 3U),
+    Ccmp = (1U << 4U),
+    Aes128Cmac = (1U << 5U),
+    Gcmp = (1U << 6U),
+    Sms4 = (1U << 7U),
+    Gcmp256 = (1U << 8U),
+    Ccmp256 = (1U << 9U),
+    // (1U << 10U) intentionally skipped
+    BipGmac128 = (1U << 11U),
+    BipGmac256 = (1U << 12U),
+    BipCmac256 = (1U << 13U),
+    GtkNotUsed = (1U << 14U),
+};
+
+/**
+ * @brief WPA encoding of IEEE 802.11 algorithm types.
+ *
+ * Values obtained from hostap/src/common/defs.h.
+ */
+enum class WpaAlgorithm : uint32_t {
+    None = 0,
+    Wep,
+    Tkip,
+    Ccmp,
+    BipCmac128,
+    Gcmp,
+    Sms4,
+    Krk,
+    Gcmp256,
+    Ccmp256,
+    BipGmac128,
+    BipGmac256,
+    BipCmac256,
+};
+
+/**
+ * @brief WPA encoding of IEEE 802.11 key management types.
+ *
+ * Values obtained from hostap/src/common/defs.h.
+ */
+enum class WpaKeyManagement : uint32_t {
+    Ieee80211x = (1U << 0U),
+    Psk = (1U << 1U),
+    None = (1U << 2U),
+    Ieee80211xNoWpa = (1U << 3U),
+    WpaNone = (1U << 4U),
+    FtIeee8021x = (1U << 5U),
+    FtPsk = (1U << 6U),
+    Ieee8021xSha256 = (1U << 7U),
+    PskSha256 = (1U << 8U),
+    Wps = (1U << 9U),
+    Sae = (1U << 10U),
+    FtSae = (1U << 11U),
+    WapiPsk = (1U << 12U),
+    WapiCert = (1U << 13U),
+    Cckm = (1U << 14U),
+    Osen = (1U << 15U),
+    Ieee80211xSuiteB = (1U << 16U),
+    Ieee80211xSuiteB192 = (1U << 17U),
+    FilsSha256 = (1U << 18U),
+    FileSha384 = (1U << 19U),
+    FtFilsSha256 = (1U << 20U),
+    FtFilsSha384 = (1U << 21U),
+    Owe = (1U << 22U),
+    Dpp = (1U << 23U),
+    FtIeee8021xSha384 = (1U << 24U),
+    Pasn = (1U << 25U),
+};
+
+static constexpr auto WpaKeyManagementFt =
+    std::to_underlying(WpaKeyManagement::Ieee80211x) |
+    std::to_underlying(WpaKeyManagement::FtIeee8021x) |
+    std::to_underlying(WpaKeyManagement::FtIeee8021xSha384) |
+    std::to_underlying(WpaKeyManagement::FtSae) |
+    std::to_underlying(WpaKeyManagement::FtFilsSha256) |
+    std::to_underlying(WpaKeyManagement::FtFilsSha384);
+
 struct MulticastListenerDiscoveryProtocolInfo
 {
     int Id;
@@ -219,6 +328,12 @@ struct ProtocolHostapd :
     static constexpr auto PropertyNameBssBssid = "bssid";
     static constexpr auto PropertyNameBssSsid = "ssid";
     static constexpr auto PropertyNameBssNumStations = "num_sta";
+
+    static constexpr auto PropertyNameWpaKeyManagement = "wpa_key_mgmt";
+    static constexpr auto PropertyNameWpaPairwise = "wpa_pairwise";
+    static constexpr auto PropertyNameWpaPassphrase = "wpa_passphrase";
+    static constexpr auto PropertyNameWpaPsk = "wpa_psk";
+    static constexpr auto PropertyNameRsnPairwise = "rsn_pairwise";
 
     // Response properties for the "STATUS" command.
     // Note: all properties must be terminated with the key-value delimeter (=).
