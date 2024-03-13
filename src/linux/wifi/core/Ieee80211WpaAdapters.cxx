9709ccea78a4a69c3f68458195aa95043f39f1f9
Andrew Beltrano <abeltrano@gmail.com>
2024-03-13 18:08:51 +0000

    Remove Wpa prefix with using decl.

--- src/linux/wifi/core/Ieee80211WpaAdapters.cxx
+++ src/linux/wifi/core/Ieee80211WpaAdapters.cxx
@@ -12,46 +12,48 @@
 
 namespace Microsoft::Net::Wifi
 {
-Wpa::HostapdHwMode
+using namespace Wpa;
+
+HostapdHwMode
 IeeeProtocolToHostapdHwMode(Ieee80211Protocol ieeeProtocol)
 {
     switch (ieeeProtocol) {
     case Ieee80211Protocol::B:
-        return Wpa::HostapdHwMode::Ieee80211b;
+        return HostapdHwMode::Ieee80211b;
     case Ieee80211Protocol::G:
-        return Wpa::HostapdHwMode::Ieee80211g;
+        return HostapdHwMode::Ieee80211g;
     case Ieee80211Protocol::N:
-        return Wpa::HostapdHwMode::Ieee80211a; // TODO: Could be a or g depending on band
+        return HostapdHwMode::Ieee80211a; // TODO: Could be a or g depending on band
     case Ieee80211Protocol::A:
-        return Wpa::HostapdHwMode::Ieee80211a;
+        return HostapdHwMode::Ieee80211a;
     case Ieee80211Protocol::AC:
-        return Wpa::HostapdHwMode::Ieee80211a;
+        return HostapdHwMode::Ieee80211a;
     case Ieee80211Protocol::AD:
-        return Wpa::HostapdHwMode::Ieee80211ad;
+        return HostapdHwMode::Ieee80211ad;
     case Ieee80211Protocol::AX:
-        return Wpa::HostapdHwMode::Ieee80211a;
+        return HostapdHwMode::Ieee80211a;
     case Ieee80211Protocol::BE:
-        return Wpa::HostapdHwMode::Ieee80211a; // TODO: Assuming a, although hostapd docs don't mention it
+        return HostapdHwMode::Ieee80211a; // TODO: Assuming a, although hostapd docs don't mention it
     default:
-        return Wpa::HostapdHwMode::Unknown;
+        return HostapdHwMode::Unknown;
     }
 }
 
 std::string
-HostapdHwModeToPropertyValue(Wpa::HostapdHwMode hwMode)
+HostapdHwModeToPropertyValue(HostapdHwMode hwMode)
 {
     switch (hwMode) {
-    case Wpa::HostapdHwMode::Ieee80211b:
-        return Wpa::ProtocolHostapd::PropertyHwModeValueB;
-    case Wpa::HostapdHwMode::Ieee80211g:
-        return Wpa::ProtocolHostapd::PropertyHwModeValueG;
-    case Wpa::HostapdHwMode::Ieee80211a:
-        return Wpa::ProtocolHostapd::PropertyHwModeValueA;
-    case Wpa::HostapdHwMode::Ieee80211ad:
-        return Wpa::ProtocolHostapd::PropertyHwModeValueAD;
-    case Wpa::HostapdHwMode::Ieee80211any:
-        return Wpa::ProtocolHostapd::PropertyHwModeValueAny;
-    default: // case Wpa::HostapdHwMode::Unknown
+    case HostapdHwMode::Ieee80211b:
+        return ProtocolHostapd::PropertyHwModeValueB;
+    case HostapdHwMode::Ieee80211g:
+        return ProtocolHostapd::PropertyHwModeValueG;
+    case HostapdHwMode::Ieee80211a:
+        return ProtocolHostapd::PropertyHwModeValueA;
+    case HostapdHwMode::Ieee80211ad:
+        return ProtocolHostapd::PropertyHwModeValueAD;
+    case HostapdHwMode::Ieee80211any:
+        return ProtocolHostapd::PropertyHwModeValueAny;
+    default: // case HostapdHwMode::Unknown
         LOGE << std::format("Invalid hostapd hw_mode value {}", magic_enum::enum_name(hwMode));
         return "invalid";
     }
@@ -62,11 +64,11 @@ IeeeFrequencyBandToHostapdBand(Ieee80211FrequencyBand ieeeFrequencyBand)
 {
     switch (ieeeFrequencyBand) {
     case Ieee80211FrequencyBand::TwoPointFourGHz:
-        return Wpa::ProtocolHostapd::PropertySetBandValue2G;
+        return ProtocolHostapd::PropertySetBandValue2G;
     case Ieee80211FrequencyBand::FiveGHz:
-        return Wpa::ProtocolHostapd::PropertySetBandValue5G;
+        return ProtocolHostapd::PropertySetBandValue5G;
     case Ieee80211FrequencyBand::SixGHz:
-        return Wpa::ProtocolHostapd::PropertySetBandValue6G;
+        return ProtocolHostapd::PropertySetBandValue6G;
     default:
         LOGE << std::format("Invalid ieee80211 frequency band value {}", magic_enum::enum_name(ieeeFrequencyBand));
         return "invalid";
