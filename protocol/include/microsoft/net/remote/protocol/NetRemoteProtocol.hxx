6a91f67cd091747d3ee8049324aa5bebd5dad93b
Andrew Beltrano <abeltrano@gmail.com>
2024-03-25 16:04:24 +0000

    Fix default protocol variables.

--- protocol/include/microsoft/net/remote/protocol/NetRemoteProtocol.hxx
+++ protocol/include/microsoft/net/remote/protocol/NetRemoteProtocol.hxx
@@ -8,9 +8,6 @@
 
 namespace Microsoft::Net::Remote::Protocol
 {
-static constexpr uint32_t NetRemotePortDefault = 5047;
-static constexpr std::string_view NetRemoteAddressHttpDefault = "localhost:5047";
-
 /**
  * @brief Static NetRemote protocol information.
  */
@@ -18,13 +15,13 @@ struct NetRemoteProtocol
 {
 #define IP_DEFAULT     "localhost"
 #define PORT_DEFAULT   5047
-#define PORT_SEPARATOR ""
+#define PORT_SEPARATOR ":"
 #define xstr(s)        str(s)
 #define str(s)         #s
 
-    static constexpr uint32_t PortDefault{ 5047 };
-    static constexpr std::string_view PortSeparator{ ":" };
-    static constexpr std::string_view IpDefault{ "localhost" };
+    static constexpr uint32_t PortDefault{ PORT_DEFAULT };
+    static constexpr std::string_view PortSeparator{ PORT_SEPARATOR };
+    static constexpr std::string_view IpDefault{ IP_DEFAULT };
     static constexpr std::string_view AddressDefault{ IP_DEFAULT PORT_SEPARATOR xstr(PORT_DEFAULT) };
 
 #undef IP_DEFAULT
