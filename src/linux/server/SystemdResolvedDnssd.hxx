91bcf39151f360fd2a58ebf544e8eeb28abf368a
Andrew Beltrano <abeltrano@gmail.com>
2024-05-18 03:55:30 +0000

    Add systemd DNS-SD constants.

--- src/linux/server/SystemdResolvedDnssd.hxx
+++ src/linux/server/SystemdResolvedDnssd.hxx
@@ -15,6 +15,16 @@ namespace Microsoft::Net::Systemd
 struct ResolvedDnssd
 {
     /**
+     * @brief The name of the D-Bus service for systemd-resolved.
+     */
+    static constexpr auto DbusServiceName = "org.freedesktop.resolve1";
+
+    /**
+     * @brief The object path for sytemd-resolved D-Bus objects.
+     */
+    static constexpr auto DbusServiceObjectPath = "/org/freedesktop/resolve1";
+
+    /**
      * @brief Build the TxtText' record for a DNS-SD service.
      *
      * @param ipAddresses The ip addresses to include in the record.
@@ -25,14 +35,14 @@ struct ResolvedDnssd
 
     /**
      * @brief Register a DNS-SD service with systemd-resolved.
-     * 
+     *
      * @param serviceName The name of the service.
      * @param protocol The protocol of the service.
      * @param port The port of the service.
      * @param txtTextRecords The TXT records for the service.
      * @param priority The optional priority of the service.
      * @param weight The optional weight of the service.
-     * @return std::string 
+     * @return std::string
      */
     static std::string
     RegisterService(std::string_view serviceName, std::string_view protocol, uint16_t port, std::vector<std::string> txtTextRecords, std::optional<uint16_t> priority, std::optional<uint16_t> weight);
