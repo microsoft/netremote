41b28ef8759210a121d997c722d28bd18de0f730
Andrew Beltrano <abeltrano@gmail.com>
2024-06-17 14:25:55 -0600

    Pass CMake build arguments to Docker build action and entrypoint. Use local Dockerfile.

--- .docker/netremote-dev/entrypoint-build.sh
+++ .docker/netremote-dev/entrypoint-build.sh
@@ -2,7 +2,22 @@
 
 set -euf -o pipefail
 
-REPOSITORY_ROOT=${1:-${PWD}}
+readonly REPOSITORY_ROOT="${1:-${PWD}}"
+readonly CMAKE_PRESET_CONFIGURE="${2}"
+readonly CMAKE_PRESET_BUILD="${3}"
+readonly CMAKE_PRESET_PACKAGE="${4}"
+readonly CMAKE_BUILD_CONFIG="${5}"
+
+echo "------------------------------------"
+echo "Build entrypoint script parameters:"
+echo "------------------------------------"
+echo "Source Repository Root=${REPOSITORY_ROOT}"
+echo "CMake"
+echo " Build Configuration=${CMAKE_BUILD_CONFIG}"
+echo " Presets"
+echo "  Configure='${CMAKE_PRESET_CONFIGURE}'"
+echo "  Build='${CMAKE_PRESET_BUILD}'"
+echo "  Package='${CMAKE_PRESET_PACKAGE}'"
 
 # Verify the repository root is a git repository.
 if [[ ! -d ${REPOSITORY_ROOT}/.git ]]; then
@@ -16,17 +31,11 @@ if [[ ! -f ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ]]; then
   exit 1
 fi
 
-# Assign cmake presets.
-# TODO: these should be passed in as arguments instead.
-PRESET_CONFIGURE=release-linux
-PRESET_BUILD=release-linux-debug
-BUILD_CONFIG=Debug
-
 # Add the workspace directory to the safe directory list.
-git config --global --add safe.directory ${REPOSITORY_ROOT}
+git config --global --add safe.directory "${REPOSITORY_ROOT}"
 
 # Change to the root of the repo.
-cd ${REPOSITORY_ROOT}
-cmake --preset ${PRESET_CONFIGURE}
-cmake --build --preset ${PRESET_BUILD}
-cmake --install out/build/${PRESET_CONFIGURE} --config ${BUILD_CONFIG}
+cd "${REPOSITORY_ROOT}"
+cmake --preset "${CMAKE_PRESET_CONFIGURE}"
+cmake --build --preset "${CMAKE_PRESET_BUILD}"
+cmake --install out/build/"${CMAKE_PRESET_CONFIGURE}" --config "${CMAKE_BUILD_CONFIG}"
