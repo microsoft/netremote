8f5e38c2bde922f997baf477073003365bd8afae
Andrew Beltrano <abeltrano@gmail.com>
2024-06-29 20:36:02 -0600

    Add ctest execution to build entrypoint.

--- .docker/netremote-dev/entrypoint-build.sh
+++ .docker/netremote-dev/entrypoint-build.sh
@@ -3,10 +3,11 @@
 set -euf -o pipefail
 
 readonly REPOSITORY_ROOT="${1:-${PWD}}"
-readonly CMAKE_PRESET_CONFIGURE="${2:-dev-linux}"
-readonly CMAKE_PRESET_BUILD="${3:-build-dev-linux}"
-readonly CMAKE_PRESET_PACKAGE="${4:-packaging-dev-linux}"
-readonly CMAKE_BUILD_CONFIG="${5:-Debug}"
+readonly CMAKE_PRESET_CONFIGURE="${2:-cicd-linux}"
+readonly CMAKE_PRESET_BUILD="${3:-build-cicd-linux}"
+readonly CMAKE_PRESET_PACKAGE="${4:-cicd-linux}"
+readonly CMAKE_PRESET_TEST="${5:-test-cicd-linux}"
+readonly CMAKE_BUILD_CONFIG="${6:-Debug}"
 
 echo "------------------------------------"
 echo "Build entrypoint script parameters:"
@@ -17,6 +18,7 @@ echo " Build Configuration=${CMAKE_BUILD_CONFIG}"
 echo " Presets"
 echo "  Configure='${CMAKE_PRESET_CONFIGURE}'"
 echo "  Build='${CMAKE_PRESET_BUILD}'"
+echo "  Test='${CMAKE_PRESET_TEST}'"
 echo "  Package='${CMAKE_PRESET_PACKAGE}'"
 
 # Verify the repository root is a git repository.
@@ -34,8 +36,8 @@ fi
 # Add the workspace directory to the safe directory list.
 git config --global --add safe.directory "${REPOSITORY_ROOT}"
 
-# Change to the root of the repo.
+# Change to the root of the repo then execute then perform CMake configure, build, and test.
 cd "${REPOSITORY_ROOT}"
 cmake --preset "${CMAKE_PRESET_CONFIGURE}"
 cmake --build --preset "${CMAKE_PRESET_BUILD}"
-ctest --preset non-root -C "${CMAKE_BUILD_CONFIG}"
+ctest --preset "${CMAKE_PRESET_TEST}" -C "${CMAKE_BUILD_CONFIG}"
