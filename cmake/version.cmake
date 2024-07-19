9002bf5b26fac4469ff215b35505ccf209cb399a
Andrew Beltrano <abeltrano@gmail.com>
2024-07-19 16:05:04 -0600

    Fix version determination.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -10,23 +10,22 @@ else ()
   message(STATUS "Using git to determine version")
 
   execute_process(
-    COMMAND "${GIT_EXECUTABLE}" describe --tags --abbrev=0
+    COMMAND "${GIT_EXECUTABLE}" tag -l --sort=-v:refname
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    RESULT_VARIABLE GIT_DESCRIBE_RESULT
-    OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT
+    RESULT_VARIABLE GIT_TAG_RESULT
+    OUTPUT_VARIABLE GIT_TAG_OUTPUT
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
-  if (GIT_DESCRIBE_RESULT EQUAL 0)
-    if (GIT_DESCRIBE_OUTPUT MATCHES "^v([0-9]+)[.]([0-9]+)[.]([0-9]+)[.]*")
+  if (GIT_TAG_RESULT EQUAL 0)
+    if (GIT_TAG_OUTPUT MATCHES "^v([0-9]+)[.]([0-9]+)[.]([0-9]+)[.]*")
       set(VERSION_MAJOR ${CMAKE_MATCH_1})
       set(VERSION_MINOR ${CMAKE_MATCH_2})
       set(VERSION_PATCH ${CMAKE_MATCH_3})
-      message(STATUS "Detected version from latest git tag: ${GIT_DESCRIBE_OUTPUT}")
+      message(STATUS "Detected version from latest git tag: v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
     else()
-      message(WARNING "Failed to get version from git: ${GIT_DESCRIBE_RESULT}; falling back to hard-coded version")
+      message(WARNING "Failed to get version from git: ${GIT_TAG_RESULT}; falling back to hard-coded version")
     endif()
   endif()
 endif()
 
-message(STATUS "Configuring netremote version v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
